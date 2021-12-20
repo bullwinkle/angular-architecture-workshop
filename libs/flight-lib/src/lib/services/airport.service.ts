973c07017d5e6f2a6de3c92597e9bac15dbbe313
Dmitriy Galnykin <developer085@gmail.com>
2021-12-20 16:02:41 +0300

    initial

---
+++ libs/flight-lib/src/lib/services/airport.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AirportService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  findAll(): Observable<string[]> {
+    const url = 'http://www.angular.at/api/airport';
+    return this.httpClient.get<string[]>(url);
+  }
+
+}

81d4c30653e0b2bac50b3dd5ed665264ef5997f1
Dmitriy Galnykin <developer085@gmail.com>
2021-12-21 17:07:09 +0300

    Using Angular Elements

--- apps/dashboard/src/app/dashboard-tile/dashboard-tile.module.ts
+++ apps/dashboard/src/app/dashboard-tile/dashboard-tile.module.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { NgModule, Injector } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Injector, NgModule } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DashboardTileComponent } from './dashboard-tile.component';
@@ -20,7 +19,8 @@ import { DashboardTileComponent } from './dashboard-tile.component';
 export class DashboardTileModule {
 
   constructor(private injector: Injector) {
-    // TODO: Wrap DashboardTileComponent as a web component and register it
+    const tileElm = createCustomElement(DashboardTileComponent, { injector: this.injector });
+    customElements.define('dashboard-tile', tileElm);
   }
 
 }
